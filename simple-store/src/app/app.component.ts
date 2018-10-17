7ade1a2cc07ce681976c21b3b09bb227d2abe38c
alexHooper <alexmohack@gmail.com>
2018-10-17 18:50:10 +0530

    change the template for app

--- simple-store/src/app/app.component.ts
+++ simple-store/src/app/app.component.ts
@@ -2,11 +2,11 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  // templateUrl: './app.component.html',
-  template: `
-  	<h1>{{ title }}</h1>
-  	<app-customers></app-customers>
-  `,
+  templateUrl: './app.component.html',
+  // template: `
+  // 	<h1>{{ title }}</h1>
+  // 	<app-customers></app-customers>
+  // `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
