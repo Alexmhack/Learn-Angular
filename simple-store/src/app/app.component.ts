c9a91b1de41770d1285e5a6060a838fbb0e08768
alexHooper <alexmohack@gmail.com>
2018-10-17 17:33:15 +0530

    using ngOnInit in class

--- simple-store/src/app/app.component.ts
+++ simple-store/src/app/app.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   // templateUrl: './app.component.html',
   template: `
-  	<h1>Simple Store Made In Angular</h1>
+  	<h1>{{ title }}</h1>
   `,
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'simple-store';
+export class AppComponent implements OnInit {
+  // title = 'simple-store angular app';
+  title: string;
+
+  constructor() { }
+
+  ngOnInit() {
+   	// we call a service that gives us the title
+   	this.title = "simple-store angular app using";
+  }
 }
