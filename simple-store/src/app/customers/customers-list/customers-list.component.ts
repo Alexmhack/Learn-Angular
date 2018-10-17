688920c7af44e14e412d0f9ab9859f3ce8c43106
alexHooper <alexmohack@gmail.com>
2018-10-17 21:14:26 +0530

    add global styles

--- simple-store/src/app/customers/customers-list/customers-list.component.ts
+++ simple-store/src/app/customers/customers-list/customers-list.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-customers-list',
   templateUrl: './customers-list.component.html',
-  styleUrls: ['./customers-list.component.css']
 })
 export class CustomersListComponent implements OnInit {
 
