e1596d7ed6aaca08329770b2020b135cd3cd203a
alexHooper <alexmohack@gmail.com>
2018-10-17 18:41:18 +0530

    create customers module file

---
+++ simple-store/src/app/customers/customers.module.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { CustomersComponent } from './customers.component';
+
+@NgModule({
+  declarations: [
+    CustomersComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+})
+export class CustomersModule { }
