871ac4d8ba47775f787a9ecddabc4d12da1989ca
alexHooper <alexmohack@gmail.com>
2018-10-17 21:04:43 +0530

    register customers-list

--- simple-store/src/app/customers/customers.module.ts
+++ simple-store/src/app/customers/customers.module.ts
@@ -2,10 +2,12 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { CustomersComponent } from './customers.component';
+import { CustomersListComponent } from './customers-list/customers-list.component';
 
 @NgModule({
   declarations: [
-    CustomersComponent
+    CustomersComponent,
+    CustomersListComponent
   ],
   imports: [
     CommonModule
