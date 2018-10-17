64b63091d4114656d021337b76e9359696bfd049
alexHooper <alexmohack@gmail.com>
2018-10-17 18:02:49 +0530

    generate customers component

--- simple-store/src/app/app.module.ts
+++ simple-store/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CustomersComponent } from './customers/customers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomersComponent
   ],
   imports: [
     BrowserModule
