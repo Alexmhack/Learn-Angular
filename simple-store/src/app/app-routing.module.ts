b7adf57822c0ea6bcb43338221cad624d0b4326e
alexHooper <alexmohack@gmail.com>
2018-10-24 19:38:31 +0530

    define base routes

--- simple-store/src/app/app-routing.module.ts
+++ simple-store/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+	{ path: '', pathMatch: 'full', redirectTo: '/customers' },
+	{ path: '**', pathMatch: 'full', redirectTo: '/customers' }
+]
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forRoot(routes)
   ],
-  declarations: []
+  declarations: [],
+  exports: [ RouterModule ]
 })
 export class AppRoutingModule { }
