5fbf6c31a326b6417566f39093997b17e76e1f3f
alexHooper <alexmohack@gmail.com>
2018-10-24 15:47:47 +0530

    add logic for services

--- simple-store/src/app/core/data.service.ts
+++ simple-store/src/app/core/data.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Observable } from 'rxjs';
+import { map, catchError } from 'rxjs/Operators';
+
+import { ICustomer, IOrder } from '../shared/interfaces';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
+  
+  baseUrl: string = 'assets/';
 
   constructor() { }
+
+  private handleError(error: any) {
+  	console.error('Server error: ', error);
+	if (error.error instanceof Error) {
+		const errorMessage = error.error.message;
+		return Observable.throw(errorMessage)
+	}
+
+	return Observable.throw(error) || 'Node.js Server Error';
+  }
 }
