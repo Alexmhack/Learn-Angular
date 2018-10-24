24e4ce9086c33a7f273a243e6a7315bd52941edf
alexHooper <alexmohack@gmail.com>
2018-10-24 16:33:14 +0530

    add functions to fetch data from server

--- simple-store/src/app/core/data.service.ts
+++ simple-store/src/app/core/data.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { map, catchError } from 'rxjs/Operators';
 
 import { ICustomer, IOrder } from '../shared/interfaces';
@@ -13,15 +13,56 @@ export class DataService {
   
   baseUrl: string = 'assets/';
 
-  constructor() { }
-
-  private handleError(error: any) {
-  	console.error('Server error: ', error);
-	if (error.error instanceof Error) {
-		const errorMessage = error.error.message;
-		return Observable.throw(errorMessage)
-	}
-
-	return Observable.throw(error) || 'Node.js Server Error';
+  constructor(private http: HttpClient) { }
+  
+  getCustomers() : Observable<ICustomer[]> {
+    return this.http.get<ICustomer[]>(this.baseUrl + 'customers.json')
+      .pipe(
+        catchError(this.handleError('getCustomers', []))
+      );
   }
+
+  getCustomer(id: number) : Observable<ICustomer> {
+    return this.http.get<ICustomer[]>(this.baseUrl + 'customers.json')
+      .pipe(
+        map(customers => {
+          let customer = customers.filter((cust: ICustomer) => cust.id === id);
+          return (customer && customer.length) ? customer[0] : null;
+        }),
+
+        catchError(this.handleError<ICustomer>('getCustomer id=${id}'))
+      );
+  }
+
+  getOrders(id: number) : Observable<IOrder[]> {
+      return this.http.get<IOrder[]>(this.baseUrl + 'orders.json')
+        .pipe(
+          map(orders => {
+            let custOrders = orders.filter((order: IOrder) => order.customerId === id);
+            return custOrders;
+          }) // ,
+          // catchError(this.orderError)
+        );
+    }
+
+  private orderError(error: any) {
+  	console.error('Server error: ', error);
+  	if (error.error instanceof Error) {
+  		const errorMessage = error.error.message;
+  		return Observable.throw(errorMessage)
+  	}
+
+  	return Observable.throw(error) || 'Node.js Server Error';
+  }
+
+  private handleError<T> (operation = 'operation', result?: T) {
+      return (error: any): Observable<T> => {
+     
+        // TODO: send the error to remote logging infrastructure
+        console.error(error); // log to console instead
+     
+        // Let the app keep running by returning an empty result.
+        return of(result as T);
+      };
+    } 
 }
